24c4fa0cbbe0b43877ccefd5a1438691cf824123
MCambie <cambie.marie@gmail.com>
2020-07-22 12:01:53 +0200

    nav et swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
-$("nav a").click(function(e){
-    e.preventDefault();
-    var lien = $(this).attr('href');
-    console.log(lien);
-    $('.content').empty();
-    $('.content').load(lien);
-});
+
+
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+  loop: true,
+    effect: 'fade',
+
+    spaceBetween: 30,
+
+    centeredSlides: true,
+
+    autoplay: {
+
+      delay: 2500,
+
+      disableOnInteraction: false,
+
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
